d6b74bccc84f6597d4fd1d056f8f0d0d4aef37c2
victor-prm <victorpabb@gmail.com>
2025-01-29 17:13:07 +0100

    added comments

--- Opgaver/Niveau1/03/script.js
+++ Opgaver/Niveau1/03/script.js
@@ -1,12 +1,15 @@
+//Listen for mouse events in body and remove an element if clicked on
 document.body.addEventListener("mousedown", (event) => {
-    console.log(event.target.id);
+    //console.log(event.target.id);
     removeElements(`#${event.target.id}`)
 })
  
+//Delete element(s) by their selector. Can also be used as standalone function.
+//Can also remove multiple elements at once e.g. by calling removeElements('article')
 function removeElements(selector) {
     let eles = document.querySelectorAll(selector);
-    console.log(eles);
     eles.forEach(el => el.remove());
 };
 
+//Alert the user with the instructions (I know this is bad practice, but it's just a homework assignment)
 alert("Instructions: Click on any element to delete it")
