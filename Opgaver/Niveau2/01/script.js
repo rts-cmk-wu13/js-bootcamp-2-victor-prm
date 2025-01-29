4e92f6f4fefe9b72a4d40890da2bf010fd7804e4
victor-prm <victorpabb@gmail.com>
2025-01-29 18:56:32 +0100

    added functionality to button selection task

---
+++ Opgaver/Niveau2/01/script.js
@@ -0,0 +1,36 @@
+let buttonContainerElement = document.querySelector("#buttons");
+let buttonsArray = Array.from(buttonContainerElement.children);
+//console.log(buttonsArray, typeof buttonsArray, buttonsArray.length);
+
+
+buttonsArray.forEach((element, i) => {
+    element.id = `buttonid-${i}`;
+    element.dataset.selected = false;
+    element.addEventListener("click", () => {
+        updateSelection(element)
+    });
+})
+
+function updateSelection(elementClicked) {
+    //console.log(elementClicked.nodeName, elementClicked.id)
+    buttonsArray.forEach((element) => {
+        isSelected = (element.dataset.selected === "true");
+       
+        if (elementClicked.id != element.id) {
+            //console.log(element.id, "set to false")
+            isSelected = false;
+        } else {
+            //console.log(element.id, "set to true")
+            isSelected = !isSelected;
+        }
+        element.dataset.selected = isSelected;
+        console.log(element.dataset.selected)
+
+        if(isSelected){
+            elementClicked.className = "selected"
+        }else{
+            element.removeAttribute("class")
+        }
+    })
+    console.log(" ");
+}
