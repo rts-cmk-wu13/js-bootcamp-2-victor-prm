ed338eced9e564f6b0017f4eafac15aadbbc8348
victor-prm <victorpabb@gmail.com>
2025-01-29 15:18:20 +0100

    updated the functionality to be more in line with the original task

--- Opgaver/Niveau1/01/js/script.js
+++ Opgaver/Niveau1/01/js/script.js
@@ -1,31 +1,52 @@
 //Find our button element in the DOM
 let buttonElement = document.querySelector('#my-button');
+//Add boolean to keep track of whether the button should be styled or not 
+let shouldColorButton = false;
 
 //Add an event listener that invokoes our "changeButtonColor"-function whenever the user clicks the button.
 buttonElement.addEventListener("click", changeButtonColor);
 
 function changeButtonColor(){
-    //Pick Random HSL Values
-    let r_h = pickRandomNumber(360);
-    let r_s = pickRandomNumber(100);
-    let r_l = pickRandomNumber(100);
+    //Invert the boolean of shouldColorButton every time the button is clicked
+    shouldColorButton = !shouldColorButton
 
-    //Compose random HSL string
-    let randomHSL = `hsl(${r_h} ${r_s}% ${r_l}%)`;
-
-    //Save a variable for the current text color of the button
-    let textColor = 'black';
-    
-    //Change the text color to white if the random HSL color is below a given threshold (60%)
-    if(r_l < 60){
-        textColor = 'white';
+    //Update or clear the style of the button
+    if(shouldColorButton){
+        doColorButton(buttonElement)
+    }else{
+        doResetStyle(buttonElement)
     }
-
-    //Update the color styles of the button element
-    buttonElement.style.backgroundColor = randomHSL;
-    buttonElement.style.color = textColor;
 }
 
+//Picking a random HSL and coloring the button
+function doColorButton(ele){
+     //Pick Random HSL Values
+     let r_h = pickRandomNumber(360);
+     let r_s = pickRandomNumber(100);
+     let r_l = pickRandomNumber(100);
+ 
+     //Compose random HSL string
+     let randomHSL = `hsl(${r_h} ${r_s}% ${r_l}%)`;
+ 
+     //Save a variable for the current text color of the button
+     let textColor = 'black';
+     
+     //Change the text color to white if the random HSL color is below a given threshold (60%)
+     if(r_l < 60){
+         textColor = 'white';
+     }
+ 
+     //Update the color styles of the button element
+     ele.style.backgroundColor = randomHSL;
+     ele.style.color = textColor;
+}
+
+//Reset element style
+function doResetStyle(ele){
+    ele.attributeStyleMap.clear();
+}
+
+//Pick a random number between 0 and max value
 function pickRandomNumber(max){
     return Math.floor(Math.random()*max);
 }
