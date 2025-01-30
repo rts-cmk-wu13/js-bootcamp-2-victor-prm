86422a72cc819c07c9f2cba04ee97dc1547ba4f3
victor-prm <victorpabb@gmail.com>
2025-01-30 12:35:46 +0100

    missing a few changes from last push

--- Opgaver/Niveau2/02/products.js
+++ Opgaver/Niveau2/02/products.js
@@ -2,7 +2,7 @@ let products = [
     {
         name: "Barcelona Chair",
         designer: "Mies van der Rohe",
-        price: 61000,
+        price: 61500,
         discount: "2380",
         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, repellat! Amet consectetur adipisicing elit.",
         imgSrc: "./assets/barcelona.webp",
@@ -12,7 +12,7 @@ let products = [
     {
         name: "Swan Chair",
         designer: "Arne Jacobsen",
-        price: 36000,
+        price: 36750,
         description: " Voluptatibus, repellat! Consectetur adipisicing. Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
         imgSrc: "./assets/swan.jpg",
         count: 0,
@@ -21,7 +21,7 @@ let products = [
     {
         name: "PK22",
         designer: "Poul Kjærholm",
-        price: 43000,
+        price: 43690,
         description: " Voluptatibus, repellat! Adipisicing. Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
         imgSrc: "./assets/pk22.png",
         count: 0,
@@ -30,7 +30,7 @@ let products = [
     {
         name: "Wegner CH28",
         designer: "Hans Wegner",
-        price: 39000,
+        price: 39350,
         discount: "8%",
         description: "Dolor sit amet consectetur adipisicing elit. Voluptatibus, repellat! Lorem Ipsum.",
         imgSrc: "./assets/ch28.jpg",
